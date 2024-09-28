d8935ee58bf80e2f60b75b454656fdb10cc677da
Arkar Myat <arkardev00@gmail.com>
2024-09-28 17:40:51 -0400

    args and opts

--- src/main.c
+++ src/main.c
@@ -1,7 +1,32 @@
 #define EXIT_SUCCESS 0;
+#define EXIT_FAILURE -1;
+#include <getopt.h>
+#include <stdbool.h>
+#include <stdio.h>
 
-int main(int argc, char *argv[])
-{
-  
+int main(int argc, char *argv[]) {
+  int c;
+  bool newfile = false;
+  char *filepath = NULL;
+
+  while ((c = getopt(argc, argv, "nf:")) != -1) {
+    switch (c) {
+    case 'n':
+      newfile = true;
+      break;
+    case 'f':
+      filepath = optarg;
+      break;
+    case '?':
+      printf("Unknown option -%c\n", c);
+      break;
+    default:
+      return EXIT_FAILURE;
+    }
+  }
+
+  printf("New file %d\n", newfile);
+  printf("New path %s\n", filepath);
+
   return EXIT_SUCCESS;
 }
